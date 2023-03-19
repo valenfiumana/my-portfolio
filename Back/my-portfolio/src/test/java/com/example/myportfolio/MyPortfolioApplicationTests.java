41318e8e40514c0b2a5d9ff08274cf534e6db0a8
valenfiumana <valen.fiumana@gmail.com>
2023-03-19 18:19:12 -0300

    Adding Front and Back separate folders

---
+++ Back/my-portfolio/src/test/java/com/example/myportfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
