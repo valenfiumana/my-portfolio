c1e7e1d4261d759ae16f81dc84d749b7be7c05bc
valenfiumana <valen.fiumana@gmail.com>
2023-03-22 11:01:32 -0300

    Changing import

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { InterceptorProvider } from './service/interceptor.service';
+import { InterceptorProvider } from 'src/app/service/interceptor.service';
 
 @NgModule({
   declarations: [
