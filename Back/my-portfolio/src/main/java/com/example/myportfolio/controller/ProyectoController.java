bafce8b48a345e52de70a640f2cecb23a2f45feb
valenfiumana <valen.fiumana@gmail.com>
2023-03-26 19:35:33 -0300

    Changing url cors

--- Back/my-portfolio/src/main/java/com/example/myportfolio/controller/ProyectoController.java
+++ Back/my-portfolio/src/main/java/com/example/myportfolio/controller/ProyectoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "https://myportfolio-frontend.web.app")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/proyectos")
 public class ProyectoController {
